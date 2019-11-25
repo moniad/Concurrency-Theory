2e6d6476bb324e90a5ca900f918e4b163fb2aecd
moniad <moni.dziedzic@gmail.com>
2019-11-25 22:41:28 +0100

    Code cleanup

--- Lab7/src/server/MethodRequest.java
+++ Lab7/src/server/MethodRequest.java
@@ -1,7 +1,6 @@
 package server;
 
 public abstract class MethodRequest {
-//        implements Callable<Integer> {
     public boolean guard() { // checks if synchronization conditions are fulfilled
         return false;
     }
